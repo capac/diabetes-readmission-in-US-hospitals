8c0b78a39c7ebd2b0da00218a6807099c826331d
capac <capac@users.noreply.github.com>
2021-01-18 20:19:09 +0000

    Increased font size, placed x-axis labels at 45 degrees and added readmitted feature

--- correlation_plot.py
+++ correlation_plot.py
@@ -11,11 +11,13 @@ work_dir = Path(home) / 'Programming/Python/machine-learning-exercises/diabetes-
 df = pd.read_csv(work_dir / 'data/df_encoded.csv')
 
 num_cols = ['time_in_hospital', 'num_lab_procedures', 'num_procedures',
-            'num_medications', 'number_diagnoses', 'service_use']
+            'num_medications', 'number_diagnoses', 'service_use', 'readmitted']
 
 corrs = df[num_cols].corr()
 
-plt.figure(figsize=(14, 10))
-sns.heatmap(corrs, cmap='RdBu_r', fmt='.3f', annot=True, annot_kws={'fontsize': 10})
+fig, axes = plt.subplots(figsize=(10, 6))
+sns.heatmap(corrs, cmap='RdBu_r', fmt='1.3f', annot=True, annot_kws={'fontsize': 10}, ax=axes)
+plt.setp(axes.get_xticklabels(), ha="right", rotation_mode="anchor", rotation=45, fontsize=10)
+plt.setp(axes.get_yticklabels(), ha="right", rotation_mode="anchor", rotation=0, fontsize=10)
 plt.title('Correlation plot of numeric features', fontsize=16)
 plt.savefig(work_dir / 'plots/corr_plot.png', dpi=288, bbox_inches='tight')
