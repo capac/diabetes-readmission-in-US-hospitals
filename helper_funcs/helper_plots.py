368064a4ce70659ef387f2e08fef5f9ca9cb2f34
capac <capac@users.noreply.github.com>
2021-01-20 19:10:04 +0000

    Changed labels to 'Not readmitted' and 'Readmitted'

--- helper_funcs/helper_plots.py
+++ helper_funcs/helper_plots.py
@@ -10,7 +10,8 @@ def conf_mx_heat_plot(conf_mx, model_name, plot_dir):
     norm_conf_mx = conf_mx/row_sums
     fig, ax = plt.subplots(1, 1, figsize=(6, 4))
     sns.heatmap(norm_conf_mx, cmap=plt.cm.coolwarm, ax=ax, square=True, vmin=0, vmax=1,
-                xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'],
+                xticklabels=['Not readmitted', 'Readmitted'],
+                yticklabels=['Not readmitted', 'Readmitted'],
                 annot=True, fmt='.3f', color='w', annot_kws={'fontsize': 10})
     plt.setp(ax.get_xticklabels(), ha="center", rotation_mode="anchor", rotation=0, fontsize=9)
     plt.setp(ax.get_yticklabels(), ha="center", rotation_mode="anchor", rotation=90, fontsize=9)
