0107c20353734a730fdbc478ecc70a96eadea429
capac <capac@users.noreply.github.com>
2021-01-26 19:15:06 +0000

    Learning curves for LR, DT and RF classifiers with error bars using cross validation scoring

--- learning_curves_plot.py
+++ learning_curves_plot.py
@@ -2,10 +2,10 @@
 
 import os
 from pathlib import Path
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
@@ -21,14 +21,19 @@ y = df.loc[:, 'readmitted']
 
 def learning_curves_data(model, X, y):
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=42)
-    train_errors, val_errors = [], []
-    for m in range(5, len(X_train), 500):
+    train_scores_means, train_scores_std = [], []
+    val_scores_means, val_scores_std = [], []
+    for m in range(6, len(X_train), 500):
         model.fit(X_train.iloc[:m], y_train.iloc[:m])
         y_train_predict = model.predict(X_train.iloc[:m])
         y_val_predict = model.predict(X_val)
-        train_errors.append(accuracy_score(y_train.iloc[:m], y_train_predict))
-        val_errors.append(accuracy_score(y_val, y_val_predict))
-    return train_errors, val_errors
+        train_scores = cross_val_score(model, X_train.iloc[:m], y_train_predict, cv=3, scoring='accuracy')
+        train_scores_means.append(train_scores.mean())
+        train_scores_std.append(train_scores.std())
+        val_scores = cross_val_score(model, X_val, y_val_predict, cv=3, scoring='accuracy')
+        val_scores_means.append(val_scores.mean())
+        val_scores_std.append(val_scores.std())
+    return train_scores_means, train_scores_std, val_scores_means, val_scores_std
 
 
 model_dict = {'Logistic regression': LogisticRegression(n_jobs=-1, solver='newton-cg'),
@@ -36,17 +41,17 @@ model_dict = {'Logistic regression': LogisticRegression(n_jobs=-1, solver='newto
               'Random forest classifier': RandomForestClassifier(n_jobs=-1, random_state=42,
                                                                  max_depth=40, n_estimators=500)}
 
-y_range_list = [[0.45, 1.05], [0.45, 1.15], [0.45, 1.25]]
 
 fig, axes = plt.subplots(1, 3, figsize=(14, 4))
-for ax, y_range, (model_name, model_instance) in zip(axes, y_range_list, model_dict.items()):
-    train_errors, val_errors = learning_curves_data(model_instance, X, y)
-    ax.plot(train_errors, 'r-+', linewidth=2, label='training')
-    ax.plot(val_errors, 'b-', linewidth=2, label='validation')
-    ax.legend(loc='upper right', fontsize=12)
-    ax.set_title('Accuracy score for {0:s}'.format(model_name.lower()), fontsize=14)
+for ax, (model_name, model_instance) in zip(axes, model_dict.items()):
+    means_std_list = learning_curves_data(model_instance, X, y)
+    train_scores_means, train_scores_std, val_scores_means, val_scores_std = means_std_list
+    x_arr = np.arange(len(train_scores_means))
+    ax.errorbar(x_arr, train_scores_means, yerr=train_scores_std, linewidth=2, label='training')
+    ax.errorbar(x_arr, val_scores_means, yerr=val_scores_std, linewidth=2, label='validation')
+    ax.legend(loc='best', fontsize=12)
+    ax.set_title('Cross validation for {0:s}'.format(model_name.lower()), fontsize=14)
     ax.set_xlabel('Training set size', fontsize=12)
-    ax.set_ylabel('Accuracy', fontsize=12)
-    ax.set_ylim(y_range)
+    ax.set_ylabel('Score', fontsize=12)
 fig.tight_layout()
-plt.savefig(work_dir / 'plots/learning_curves_plot.png', dpi=288, bbox_inches='tight')
+plt.savefig(work_dir / 'plots/learning_curves_err_plot.png', dpi=288, bbox_inches='tight')
