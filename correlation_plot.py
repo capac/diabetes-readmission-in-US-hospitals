8bb11cadd350c82bb23b066fb6b80ff015d3fe48
capac <capac@users.noreply.github.com>
2023-06-28 09:18:38 +0100

    Correlation plot of numeric features

--- correlation_plot.py
+++ correlation_plot.py
@@ -5,18 +5,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-work_dir = Path.home() / 'Programming/Python/machine-learning-exercises/uci-ml-repository/diabetes-in-130-US-hospitals'
-df = pd.read_csv(work_dir / 'data/df_encoded.csv')
+work_dir = Path.home() / 'Programming/Python/machine-learning-exercises/'\
+                         'uci-ml-repository/diabetes-in-130-US-hospitals'
+df = pd.read_csv(work_dir / 'data/diabetic_data.csv')
 
 num_cols = ['time_in_hospital', 'num_lab_procedures', 'num_procedures',
-            'num_medications', 'number_diagnoses', 'service_use', 'readmitted']
+            'num_medications', 'number_outpatient', 'number_emergency',
+            'number_inpatient', 'number_diagnoses']
 
-corrs = df[num_cols].corr()
+# print(df.select_dtypes(np.number).columns.to_list())
+# df['race'] = df['race'].factorize()[0]
+# df['readmitted'] = df['readmitted'].factorize()[0]
+corrs = df[num_cols].corr(numeric_only=True)
 
 fig, axes = plt.subplots(figsize=(10, 6))
-sns.heatmap(corrs, cmap='RdBu_r', fmt='1.3f', annot=True, annot_kws={'fontsize': 10}, ax=axes)
-plt.setp(axes.get_xticklabels(), ha="right", rotation_mode="anchor", rotation=45, fontsize=10)
-plt.setp(axes.get_yticklabels(), ha="right", rotation_mode="anchor", rotation=0, fontsize=10)
-plt.title('Correlation plot of numeric features with readmission', fontsize=16)
+sns.heatmap(corrs, cmap='RdBu_r', fmt='1.3f', annot=True,
+            annot_kws={'fontsize': 10}, ax=axes)
+plt.setp(axes.get_xticklabels(), ha="right", rotation_mode="anchor",
+         rotation=45, fontsize=10)
+plt.setp(axes.get_yticklabels(), ha="right", rotation_mode="anchor",
+         rotation=0, fontsize=10)
+plt.title('Correlation plot of numeric features', fontsize=16)
 fig.tight_layout()
-plt.savefig(work_dir / 'plots/corr_plot.png', dpi=288, bbox_inches='tight')
+plt.savefig(work_dir / 'plots/corr_plot.png', dpi=288)
