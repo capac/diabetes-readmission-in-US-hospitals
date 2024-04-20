cfdeb909224337d79009ba88aeb1fed9d20a6742
capac <capac@users.noreply.github.com>
2024-04-20 10:29:07 +0100

    Hyperparameter optimization on entire dataset

--- grid_search.py
+++ grid_search.py
@@ -27,38 +27,9 @@ df[cat_list] = df[cat_list].astype("object")
 
 X = df.drop('readmitted', axis=1)
 y = df['readmitted'].copy()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
-                                                    random_state=42)
-
-
-def preprocessing_data(X_train, X_test):
-    # standardize numeric data and generate one-hot encoded data features
-    num_pipeline = make_pipeline(StandardScaler())
-    cat_pipeline = make_pipeline(OneHotEncoder(
-        handle_unknown="ignore"),)
-
-    # preprocessing pipeline
-    preprocessing = make_column_transformer(
-        (num_pipeline, make_column_selector(dtype_include=np.number)),
-        (cat_pipeline, make_column_selector(dtype_include="object"),),
-        sparse_threshold=0,)
-
-    # output dataframe from preprocessing pipeline
-    X_train_pp = preprocessing.fit_transform(X_train)
-    pp_columns = preprocessing.get_feature_names_out()
-    X_test_pp = preprocessing.transform(X_test)
-    X_train_pp = pd.DataFrame(X_train_pp, columns=pp_columns,
-                              index=X_train.index,)
-    X_test_pp = pd.DataFrame(X_test_pp, columns=pp_columns,
-                             index=X_test.index,)
-    return X_train_pp, X_test_pp
-
-
-X_train_pp, X_test_pp = preprocessing_data(X_train, X_test)
-
 
 rus = RandomUnderSampler(sampling_strategy="majority", random_state=0)
-X_train_resampled, y_train_resampled = rus.fit_resample(X_train_pp, y_train)
+X_train_resampled, y_train_resampled = rus.fit_resample(X, y)
 
 
 param_grid = {'max_depth': np.arange(4, 20, 4),
