65fc3aee4e45d1885a1c2fc82529d81511ccd629
capac <capac@users.noreply.github.com>
2024-04-27 16:05:16 +0100

    Updated to pandas 2.2.x

--- exploratory_data_analysis/a1c_diagnosis_readmitted.py
+++ exploratory_data_analysis/a1c_diagnosis_readmitted.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 work_dir = Path.home() / 'Programming/Python/machine-learning-exercises/'\
-                        'uci-ml-repository/diabetes-in-130-US-hospitals/'
+                         'uci-ml-repository/diabetes-in-130-US-hospitals/'
 data_file = work_dir / 'data/diabetic_data.csv'
 
 # bar plot style with directory path
@@ -15,32 +15,52 @@ plt.style.use(barplot_style)
 
 df = pd.read_csv(data_file, na_values='?', low_memory=False)
 obj_cols = df.select_dtypes('object').columns
-df[obj_cols] = df[obj_cols].astype('category')
+df[obj_cols] = df[obj_cols].astype('string')
 
-# A1Cresult
-a1c_result_df = df['A1Cresult'].value_counts(dropna=False, normalize=True)*100
-a1c_result_df = a1c_result_df.reset_index().rename(columns={'index': 'Feature', 'A1Cresult': 'Percentage'})
-
-# readmitted
-readmitted_df = df['readmitted'].value_counts(dropna=False, normalize=True)*100
-readmitted_df = readmitted_df.reset_index().rename(columns={'index': 'Feature', 'A1Cresult': 'Percentage'})
-
-# primary diagnosis
+# removing NaN or null values from race and gender
 df = df.loc[df.race.notnull()]
 df = df.loc[df.gender != 'Unknown/Invalid', :]
 
-df.drop(['weight', 'medical_specialty', 'payer_code', 'encounter_id'], axis=1, inplace=True)
+# dropping categories due to high presence of NaN / null values
+df.drop(['weight', 'medical_specialty', 'payer_code', 'encounter_id'],
+        axis=1, inplace=True)
 
+# change np.nan to 'None' in 'max_glu_serum' and 'A1Cresult' columns
+df[['max_glu_serum', 'A1Cresult']] = \
+    df[['max_glu_serum', 'A1Cresult']].fillna('None')
+
+# A1Cresult
+a1c_result_df = df['A1Cresult'].value_counts(dropna=False, normalize=True)*100
+a1c_result_df = a1c_result_df.reset_index().\
+    rename(columns={'A1Cresult': 'Feature', 'proportion': 'Percentage'})
+
+# readmitted
+readmitted_df = df['readmitted'].value_counts(dropna=False, normalize=True)*100
+readmitted_df = readmitted_df.reset_index().\
+    rename(columns={'readmitted': 'Feature', 'proportion': 'Percentage'})
+readmitted_df.replace(to_replace={'NO': 'No'}, inplace=True)
+
+# converting null/unmapped values in 'admission_type_id',
+# 'discharge_disposition_id' and 'admission_source_id' to NaN.
 df['admission_type_id'] = df['admission_type_id'].replace([5, 6, 8], np.nan)
-df['discharge_disposition_id'] = df['discharge_disposition_id'].replace([18, 25, 26], np.nan)
-df['admission_source_id'] = df['admission_source_id'].replace([9, 15, 17, 20, 21], np.nan)
-id_list = ['admission_type_id', 'discharge_disposition_id', 'admission_source_id']
-df[id_list] = df[id_list].astype('category')
+df['discharge_disposition_id'] = df['discharge_disposition_id'].\
+    replace([18, 25, 26], np.nan)
+df['admission_source_id'] = df['admission_source_id'].\
+    replace([9, 15, 17, 20, 21], np.nan)
 
+# converting 'admission_type_id', 'discharge_disposition_id'
+# and 'admission_source_id' from int64 to string
+id_list = ['admission_type_id', 'discharge_disposition_id',
+           'admission_source_id']
+df[id_list] = df[id_list].astype('string')
+
+# consolidating all variations of `Expired at...` and removing them
 df = df[~df['discharge_disposition_id'].isin([11, 19, 20, 21])]
 
-df['service_use'] = df['number_outpatient'] + df['number_emergency'] + df['number_inpatient']
-df.drop(['number_outpatient', 'number_emergency', 'number_inpatient'], axis=1, inplace=True)
+df['service_use'] = df['number_outpatient'] +\
+    df['number_emergency'] + df['number_inpatient']
+df.drop(['number_outpatient', 'number_emergency', 'number_inpatient'],
+        axis=1, inplace=True)
 
 df.dropna(how='any', inplace=True)
 
@@ -52,39 +72,48 @@ musculoskeletal_list = [str(f) for f in list(range(710, 740))]
 genitourinary_list = [str(f) for f in list(range(580, 630)) + [788]]
 neoplasms_list = [str(f) for f in list(range(140, 240))]
 
-diagnosis_list = [circulatory_list, respiratory_list, digestive_list, injury_list,
-                  musculoskeletal_list, genitourinary_list, neoplasms_list]
+diagnosis_list = [circulatory_list, respiratory_list,
+                  digestive_list, injury_list,
+                  musculoskeletal_list, genitourinary_list,
+                  neoplasms_list]
 diagnosis_names = ['Circulatory', 'Respiratory', 'Digestive', 'Injury',
                    'Musculoskeletal', 'Genitourinary', 'Neoplasms']
 
-for diag_name, diag_list in zip(diagnosis_names, diagnosis_list):
-    df.loc[:, diag_name+'_col'] = np.array([np.nan for i in range(df.shape[0])])
-    filter_ = df['diag_1'].isin(diag_list)
-    df.loc[filter_, diag_name+'_col'] = np.array([diag_name for i in range(filter_.sum())])
+# selection on primary diagnosis
+diag_col, index = 'diag_1', 1
+df['primary_diag'] = np.array(['Others' for _ in range(df.shape[0])],
+                              dtype='object')
 
+for diag_name, diag_list in zip(diagnosis_names, diagnosis_list):
+    mask = df[diag_col].isin(diag_list)
+    df.loc[mask, 'primary_diag'] = \
+        np.array([diag_name for _ in range(mask.sum())], dtype='object')
+
+# other diagnosis
 diab_others_list = ['Diabetes', 'Others', 'Others']
 char_list = ['250.', 'E', 'V']
 
 for diag_name, char in zip(diab_others_list, char_list):
-    df[diag_name+'_col'] = np.array([np.nan for _ in range(df.shape[0])])
-    filter_ = df['diag_1'].str.contains(char)
-    df.loc[filter_, diag_name+'_col'] = np.array([diag_name for i in range(filter_.sum())])
+    mask = df[diag_col].str.contains(char)
+    df.loc[mask, 'primary_diag'] = \
+        np.array([diag_name for _ in range(mask.sum())])
 
-tmp_diag_list = [col+'_col' for col in diagnosis_names + ['Diabetes', 'Others']]
-df['primary_diag'] = df[tmp_diag_list].fillna(axis=1, method='bfill').iloc[:, 0]
-df['primary_diag'].fillna(value='Others', inplace=True)
-df['primary_diag'] = df['primary_diag'].astype('category')
-df.drop(tmp_diag_list, axis=1, inplace=True)
+# dropping all three diagnosis columns
+df.drop(['diag_1', 'diag_2', 'diag_3'], axis=1, inplace=True)
 
-diagnosis_df = df['primary_diag'].value_counts(dropna=False)
+diagnosis_df = df['primary_diag'].value_counts(dropna=False,
+                                               normalize=True)*100
 diagnosis_df.sort_values(ascending=False, inplace=True)
-diagnosis_df = (diagnosis_df/diagnosis_df.sum()*100).apply('{:.2f}'.format).astype('float')
-diagnosis_df = diagnosis_df.reset_index().rename(columns={'index': 'Primary diagnosis', 'primary_diag': 'Percentage'})
+
+diagnosis_df = diagnosis_df.reset_index().\
+    rename(columns={'primary_diag': 'Primary diagnosis',
+                    'proportion': 'Percentage'})
 
 order_list = ['Circulatory', 'Respiratory', 'Digestive', 'Diabetes', 'Injury',
               'Musculoskeletal', 'Genitourinary', 'Neoplasms', 'Others']
 diagnosis_df = diagnosis_df.set_index('Primary diagnosis').loc[order_list]
-diagnosis_df = diagnosis_df.reset_index().rename(columns={'Primary diagnosis': 'Feature'})
+diagnosis_df = diagnosis_df.reset_index().\
+    rename(columns={'Primary diagnosis': 'Feature'})
 
 df_list = [a1c_result_df, diagnosis_df, readmitted_df]
 title_list = ['Percentage of HbA1c measurements',
