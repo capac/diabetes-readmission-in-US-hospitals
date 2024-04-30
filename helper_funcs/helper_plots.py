f787104678cfb60c9c2d39f90d65298bf33a7df6
capac <capac@users.noreply.github.com>
2024-04-30 12:17:56 +0100

    Changed font size for ticks and labels

--- helper_funcs/helper_plots.py
+++ helper_funcs/helper_plots.py
@@ -9,6 +9,8 @@ column_length_in_px = 4
 nrows = 1
 ncols = 3
 
+plt.style.use("lineplot-style.mplstyle")
+
 
 def roc_curve_plot_with_auc(rates_dict, work_dir):
     fig, axes = plt.subplots(nrows, ncols, figsize=(row_length_in_px,
@@ -19,19 +21,21 @@ def roc_curve_plot_with_auc(rates_dict, work_dir):
         ax.plot(fpr, tpr, 'b.-', label='AUC: {:.4f}'.format(model_roc_auc))
         ax.plot([0, 1], [0, 1], 'r--')
         ax.set_xlim([-0.02, 1.0])
-        plt.setp(ax.get_xticklabels(), fontsize=10)
         ax.set_ylim([0.0, 1.02])
+        plt.setp(ax.get_xticklabels(), fontsize=10)
         plt.setp(ax.get_yticklabels(), fontsize=10)
         ax.set_xlabel('False Positive Rate', fontsize=10)
         ax.set_ylabel('True Positive Rate', fontsize=10)
         if len(model_name) > 3:
-            ax.set_title(f'ROC curve for {model_name.lower()}', fontsize=11)
+            ax.set_title(f'ROC curve for {model_name.lower()}', fontsize=10)
         else:
-            ax.set_title(f'ROC curve for {model_name.upper()}', fontsize=11)
+            ax.set_title(f'ROC curve for {model_name.upper()}', fontsize=10)
         ax.legend(loc='lower right', fontsize=12)
-    fig.suptitle('Receiver operating characteristic curves', fontsize=12)
+    fig.suptitle('Receiver operating characteristic curves',
+                 fontsize=11, fontweight='bold')
     fig.tight_layout()
-    plt.savefig(work_dir / 'plots/auc_plots.png', dpi=288, bbox_inches='tight')
+    plt.savefig(work_dir / 'plots/auc_plots.png',
+                bbox_inches='tight')
 
 
 def conf_mx_heat_plot(cm_dict, work_dir):
@@ -51,16 +55,16 @@ def conf_mx_heat_plot(cm_dict, work_dir):
                  rotation=0, fontsize=9)
         plt.setp(ax.get_yticklabels(), ha="center", rotation_mode="anchor",
                  rotation=90, fontsize=9)
-        plt.tick_params(which='both', bottom=False, left=False)
         cbar = ax.collections[0].colorbar
         cbar.ax.tick_params(labelsize=8)
+        plt.tick_params(which='both', bottom=False, left=False)
         if len(model_name) > 3:
-            ax.set_title(f'Heatmap for {model_name.lower()}', fontsize=11)
+            ax.set_title(f'Heatmap for {model_name.lower()}', fontsize=10)
         else:
-            ax.set_title(f'Heatmap for {model_name.upper()}', fontsize=11)
+            ax.set_title(f'Heatmap for {model_name.upper()}', fontsize=10)
         ax.set_xlabel('Predicted values', fontsize=10)
         ax.set_ylabel('Actual values', fontsize=10)
-    fig.suptitle('Confusion matrix heatmaps', fontsize=12)
-    fig.tight_layout()
+    fig.suptitle('Confusion matrix heatmaps', fontsize=11, fontweight='bold')
+    # fig.tight_layout()
     plt.savefig(work_dir / 'plots/confusion_matrix_plots.png',
-                dpi=288, bbox_inches='tight')
+                bbox_inches='tight')
